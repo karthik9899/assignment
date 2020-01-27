c2b7ea205d232134b5ab99a6d9919d468b48a745
karthik9899 <karthikamaresh98@gmail.com>
2020-01-27 10:29:08 +0530

    name

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log("hiii")
+let firstName ="karthik"
+console.log(firstName)
+let firstName1 ="yogi miss you"
+typeof(firstName1)
+function callname(a)
+{
+    console.log(a)
+    console.log(firstName1)  
+}
+callname(10)
+function add(){
+let num1=document.getElementById("1").value
+let num2=document.getElementById("2").value
+final1=Number(num1)+Number(num2)
+console.log(final1)
+}
+function sub(){
+    let num1=document.getElementById("1").value
+    let num2=document.getElementById("2").value
+    final1=num1-num2
+    console.log(final1)
+    }
+    function mul(){
+        let num1=document.getElementById("1").value
+        let num2=document.getElementById("2").value
+        final1=num1*num2
+        console.log(final1)
+        }
+        function divi(){
+            let num1=document.getElementById("1").value
+            let num2=document.getElementById("2").value
+            final1=num1/num2
+            console.log(final1)
+            }
